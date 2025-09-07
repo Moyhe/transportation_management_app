f15f6dc1b0b52c7c11127bb663d63d7291778bac
Mohye-Mahmoud <mohye@storeus.com>
2025-09-07 20:07:36 +0300

    create trips migration table

---
+++ database/migrations/2025_09_07_165548_create_trips_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\Company;
+use App\Models\Driver;
+use App\Models\vehicle;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Vehicle::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Driver::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Company::class)->constrained()->cascadeOnDelete();
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
